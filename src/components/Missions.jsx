275690119e4942fa834cbde08178f185c9df89b2
rodpedrassi <rodpedrassi@gmail.com>
2022-07-20 17:38:23 -0300

    requisito 10 feito e projeto finalizado

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
+    const missionsHtml = missions.map((mission) => (<MissionCard
+      key={ mission.name }
+      name={ mission.name }
+      year={ mission.year }
+      country={ mission.country }
+      destination={ mission.destination }
+    />));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missionsHtml}
       </div>
     );
   }
